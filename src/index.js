9d7660b4e59a52cdbee9d542ca3da9ca1dafabd5
Khoi01~ <nhhoangkhoi@gmail.com>
2023-08-10 10:59:09 +0700

    refactor ReduxToolkit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 import App from "./App";
+import "./App.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
